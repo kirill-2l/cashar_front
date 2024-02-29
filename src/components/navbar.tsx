05379ef5ab68fca8dc28f5aca24e99f59093b8ea
Petrov Kirill <petrovkirill@gmail.com>
2024-02-29 20:59:50 -0300

    feat: navbar init

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,45 +1,115 @@
 import {
-  Box,
   Flex,
+  IconButton,
+  Button,
+  Switch,
+  useColorMode,
   Image,
-  useBreakpointValue,
-  useColorModeValue,
-  Text,
-  Img,
+  Container,
 } from "@chakra-ui/react";
-import { Link } from "@chakra-ui/next-js";
-import { DesktopNav } from "@/components/desktopNav";
+import { Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useState } from "react";
+import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 
 export function Navbar() {
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+  const [isMenuShown, toggleIsMenuShown] = useState(false);
+  const menu = [
+    {
+      label: "Банки",
+      href: "banks",
+      id: "banks",
+    },
+    {
+      label: "Дебетовые карты",
+      href: "cards",
+      id: "debit_card",
+    },
+    {
+      label: "Обменники",
+      href: "exchangers",
+      id: "exchangers",
+    },
+  ];
+
   return (
-    <Box>
+    <Container maxW="container.xl">
       <Flex
-        bg={useColorModeValue("gray.600", "gray.800")}
-        color={useColorModeValue("gray.600", "white")}
-        minH={"60px"}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1}
-        borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.900")}
-        align={"center"}
+        top="1rem"
+        right="1rem"
+        align="center"
+        justifyContent="space-between"
       >
-        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
-          <Box>
-            <Img
+        {/* Desktop */}
+        <Flex>
+          <Link href="/">
+            <Image
+              src="/logo.png"
               borderRadius="full"
               boxSize="50px"
-              src="./logo.png"
-              alt="Cashar"
-            ></Img>
-          </Box>
+              alt="Picture of the author"
+            />
+          </Link>
         </Flex>
-        <Flex display={{ base: "none", md: "flex" }} ml={10}>
-          <DesktopNav />
+        <Flex hideBelow="md" alignItems="center">
+          {menu.map((i) => (
+            <Link key={i.id} href={i.href} as={NextLink}>
+              <Button variant="ghost" aria-label={i.id} my={5} w="100%">
+                {i.label}
+              </Button>
+            </Link>
+          ))}
+          <Switch color="green" isChecked={isDark} onChange={toggleColorMode} />
         </Flex>
 
-        <Flex display={{ base: "none", md: "flex" }} ml={10}></Flex>
+        {/* Mobile */}
+        <IconButton
+          aria-label="Open Menu"
+          size="lg"
+          mr={2}
+          icon={<HamburgerIcon />}
+          onClick={() => toggleIsMenuShown(!isMenuShown)}
+          display={["flex", "flex", "none", "none"]}
+        />
       </Flex>
-    </Box>
+
+      {/* Mobile Content */}
+      <Flex
+        w="100vw"
+        display={isMenuShown ? "flex" : "none"}
+        bgColor="gray.50"
+        zIndex={20}
+        h="100vh"
+        pos="fixed"
+        top="0"
+        left="0"
+        overflowY="auto"
+        flexDir="column"
+      >
+        <Flex justify="flex-end">
+          <IconButton
+            mt={2}
+            mr={2}
+            aria-label="Open Menu"
+            size="lg"
+            icon={<CloseIcon />}
+            onClick={() => toggleIsMenuShown(!isMenuShown)}
+          />
+        </Flex>
+
+        <Flex flexDir="column" align="center">
+          {menu.map((i) => (
+            <Link key={i.id} href={i.href} as={NextLink}>
+              <Button variant="ghost" aria-label="Home" my={5} w="100%">
+                {i.label}
+              </Button>
+            </Link>
+          ))}
+          <Switch color="green" isChecked={isDark} onChange={toggleColorMode} />
+        </Flex>
+      </Flex>
+    </Container>
   );
 }
